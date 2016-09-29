b0f7d69f8203ec649dcc5d9bdad49832f3d87bc9
bgveenstra <b.g.veenstra@gmail.com>
2016-09-29 05:19:03 -0700

    31-5

--- db/migrate/20160929114150_create_users.rb
+++ db/migrate/20160929114150_create_users.rb
@@ -1,10 +1,10 @@
-class CreateUsers < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :email
       t.string :password_digest
 
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151218121307) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20160929114206) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +18,7 @@ ActiveRecord::Schema.define(version: 20151218121307) do
     t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
